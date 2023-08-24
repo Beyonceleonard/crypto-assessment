40ecf4e69ce562b3cd40d8555bb3b6b9dc50b0f9
Beyonce Leonard <beyonceleonard65@gmail.com>
2023-08-24 15:53:26 +0200

    second

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -20,36 +20,114 @@
         <li>Say "Hi" to us in the WhatsApp chat to start the conversation</li>
         <li>Follow the prompts to complete your request</li>
       </ul>
+      <div class="image">
+        <img src="https://i.postimg.cc/05SyB9nY/Sharenet-Whats-App-1eb8ee79.png">
+      </div>
     </div>
   </div>
-  <div class="container">
-    <form action="https://formspree.io/f/xlevpezg" method="post" role="form" class="formspree-form">
-<label for="title">Title</label>
-<select name="title" id="title">
-  <option value="Mr">Mr</option>
-  <option value="Mrs">Ms</option>
-  <option value="Mx">Ms</option>
-  <option value="Dr">Dr</option>
-</select>
- <input type="text" name="name" class="form-control" placeholder="Your Name" required>
- <input type="email" name="email" class="form-control" placeholder="Your Email" required>
- <input type="text" name="subject" class="form-control" placeholder="Subject" required>
- <textarea name="message" rows="8" placeholder="Your Message" required></textarea>
- <div class="text-center"><button type="submit">Submit</button></div>
-                  
-
-  </form>
-  </div>
+  <div class="con">
+            <div class="form">
+            <div class="contact-form">
+              <h3><span>COMPLETE THE FORM </span>BELOW AND WE WILL GET BACK TO YOU</h3>
+                <form action="https://formspree.io/f/moqzlyyd" method="POST">
+                    <div class="input-box">
+                        <input type="text" name="name" class="input">
+                        <label for="">FirstName:</label>
+                    </div>
+                    <div class="input-box">
+                        <input type="text" name="name" class="input">
+                        <label for="">LastName:</label>
+                    </div>
+                    <div class="input-box">
+                        <input type="email" name="email" class="input">
+                        <label for="">Email:</label>
+                    </div>
+                    <div class="input-box">
+                        <input type="tel" name="phone" class="input">
+                        <label for="">Cellphone:</label>
+                    </div>
+                    <div class="input-box textarea" >
+                        <textarea name="message" class="input"></textarea>
+                        <label for="">Message:</label>
+                    </div>
+                    <input type="submit" value="Send" class="btn-dark">
+                </form>
+            </div>
+        </div>
+     </div>
+ </div>
+ <div class="map">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.495128478341!2d18.627532676605476!3d-33.876900019480125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc41e6218f7ab3%3A0xbdd802ca53b92d43!2sSharenet%20(Pty)%20Ltd!5e0!3m2!1sen!2sza!4v1683021933066!5m2!1sen!2sza"></iframe>
+ </div>
+ <div class="subtitle">
+  <h3>FIND US <span>HERE</span></h3>
+ </div>
+ <div class="heading">
+  <p>Head Office</p>
+  <p>301, 3rd Floor Imperial Terraces
+    <br>
+    Tyger Waterfront
+    <br>
+    Carl Cronje Drive
+    <br>
+    Bellville, Cape Town
+    <br>
+    7530
+  </p>
+ </div>
+ <div class="tel">
+  <p>Telephone</p>
+  <p><a href="tel: +27217004800">(021) 700 4800</a>
+    <br>
+    <a href="tel: +27217004800"> +27 21 700 4800</a>
+  </p>
+ </div>
+ <div class="email">
+  <p>Email</p>
+  <p><a href="mailto: support@sharenet.co.za"> support@sharenet.co.za</a>
+  </p>
  </div>
 </template>
 
 <style scoped>
 *{
   box-sizing: border-box;
-
+}
+.map{
+  display: flex;
+  margin-top: 5rem;
+  margin-left: 20px;
+  height: 400px;
+}
+.subtitle h3 span{
+  color: #71b5fc;
+}
+.subtitle{
+display: block;
+margin-right: -600px;
+margin-top: -400px;
+}
+.heading {
+display: flex;
+justify-content: right;
+gap: 40px;
+margin-right: 210px;
+}
+.tel{
+  display: flex;
+  justify-content: right;
+  gap: 55px;
+  margin-right: 310px;
+}
+.email{
+  display: flex;
+  justify-content: right;
+  gap: 80px;
+  margin-right: 260px;
 }
 .container{
   width: 100%;
+  height: 440px;
   padding-right: var(--bs-gutter-x,.75rem);
   padding-left: var(--bs-gutter-x,.75rem);
   margin-right: auto;
@@ -77,31 +155,91 @@
   color: rgb(36, 220, 36);
 
 }
-.container{
-  display: flex;
-  flex-direction: column;
-  width: 100%;
+.con{
+    position: relative;
+    width:600px;
+    height: 500px;
+    margin-right: 50px;
+    border-radius: 5px;
 }
-.contact .formspree-form .form-group{
-    padding-bottom: 8px;
+.form{
+    padding: 3.3rem 2.2rem;
+    z-index: 10;
+    overflow: hidden;
+    position:relative;
 }
-.formspree-form input{
-    height: 17px;
-    border-radius: 6px;
-} .formspree-form input,
- .formspree-form textarea{
-    padding: 10px 12px;
+.input-box{
+    position: relative;
+    margin: 1rem 0;
 }
- .formspree-form button[type="submit"]{
-    background: #149ddd;
-    border: 0;
-    padding: 10px 12px;
-    color: #fff;
-    transition: 0.4s;
-    border-radius: 4px;
+
+.input{
+   width: 500px;
+   outline: none; 
+   border: 2px solid #67eb50f2;
+   background: none;
+   padding: 0.6rem 5rem;
+   color: #000000;
+   font-size: 0.95rem;
+   font-weight: 500;
+   letter-spacing: 0.5px;
+   border-radius: 10px;
+   transition: 0.4s;
 }
-.contact .formspree-form button[type="submit"]:hover{
-    background: #004e72;
+textarea.input{
+    padding: 1.8rem 0.2rem;
+    min-height: 60px;
+    border-radius: 11px;
+    resize: none;
+    overflow-y: auto;
+}
+.input-box label{
+    position: absolute;
+    top: 50%;
+    left: 15px;
+    transform: translateY(-50%);
+    padding: 0.0.4rem;
+    color: #000000;
+    font-size: 0.9rem;
+    font-weight: 400;
+    pointer-events: none;
+    z-index: 1000;
+    transition: 0.6s;
+}
+.input-box.textarea label{
+    top: 1rem;
+    transform: translateY(0);
+}
+.btn-dark{
+  padding: 0.6rem 1.3rem;
+    background-color: #113145;
+    border: 2px solid #67eb50f2;
+    font-size: 0.95rem;
+    color: #fbf7f7;
+    line-height: 1;
+    border-radius: 15px;
+    width: 180px;
+    height: 35px;
+    outline: none;
+    cursor: pointer;
+    transition: 0.3s;
+    margin: 0;
+}
+.btn-dark:hover{
+    background-color:#67eb50f2;
+    color: #000000f2;
+}
+.contact-form h3 span{
+  color: black;
+}
+.contact-form h3{
+  color: #686363;
+  font-size: 25px;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 100;
+}
+.content {
+  margin-right: 90px;
 }
 
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,32 @@
 <template>
+  <div class="background">
+    <img src="https://i.postimg.cc/HsRxZVD9/background-1.png">
+  </div>
   <div class="container">
     <table class="crypto-coin">
       <thead>
         <tr>
-          <th onclick="sortTable(0)">Rank</th>
-          <th onclick="sortTable(1)">CoinSymbol</th>
-          <th onclick="sortTable(2)">CoinName</th>
-          <th onclick="sortTable(3)">Price</th>
-          <th onclick="sortTable(4)">1h</th>
-          <th onclick="sortTable(5)">Market Cap</th>
+          <th @onclick="sortTable('Rank')">Rank</th>
+          <th @onclick="sortTable('CoinName')">CoinName</th>
+          <th @onclick="sortTable('CoinSymbol')">CoinSymbol</th>
+          <th @onclick="sortTable('Price')">Price</th>
+          <th @onclick="sortTable('price_change_percentage_1h_in_currency')">1h</th>
+          <th @onclick="sortTable('Market Cap')">Market Cap</th>
         </tr>
       </thead>
-      <tbody id="body"></tbody>
+      <tbody id="body">
+        <tr v-for="crypto in sortedCrypto" :key="crypto.id">
+            <td class="rank">{{ crypto.rank }}</td>
+            <td>
+              <img :src="crypto.image" :alt="crypto.name" class="coin-icon" />
+              {{ crypto.name }}
+            </td>
+            <td>{{ crypto.symbol }}</td>
+            <td>{{ 'ZAR ' + crypto.current_price.toFixed(2) }}</td>
+            <td>{{ crypto.price_change_percentage_1h_in_currency }}</td>
+            <td>{{ 'ZAR ' + crypto.market_cap.toLocaleString() }}</td>
+          </tr>
+      </tbody>
     </table>
   </div>
 </template>
@@ -20,43 +35,100 @@
 import axios from 'axios';
 import { computed } from 'vue';
 export default {
- data() {
-  return{
-    cryptoData: [],
-    sortKey: '',
-    sortOrder: 1,
-  };
- },
- computed: {
-  sortedCrypto() {
-    const sorted = this.cryptoData.slice().sort((a, b) => {
+  data() {
+      return {
+        cryptoData: [],
+        sortKey: '',
+        sortTable: 1,
+      };
+    },
+    computed: {
+      sortedCrypto() {
+        const sorted = this.cryptoData.slice().sort((a, b) => {
           const keyA = a[this.sortKey];
           const keyB = b[this.sortKey];
-          return this.sortOrder * (keyA > keyB ? 1 : -1);
+          return this.sortTable * (keyA > keyB ? 1 : -1);
         });
-  }
-
- }
+        return sorted;
+      },
+    },
+    methods: {
+      async fetchData() {
+        try {
+          const response = await axios.get(
+            'https://api.coingecko.com/api/v3/coins/markets',
+            {
+              params: {
+                vs_currency: 'zar',
+                ids: 'bitcoin,ethereum,ripple,cardano,polkadot,binancecoin,dogecoin,litecoin,uniswap,avalanche-2',
+                order: 'market_cap_desc',
+                per_page: 10,
+                page: 1,
+                sparkline: false,
+                localization: false,
+              },
+            }
+          );
+          this.cryptoData = response.data;
+          const coinIds = this.cryptoData.map((crypto) => crypto.id).join(',');
+          const metadataResponse = await axios.get(
+            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinIds}`
+          );
+          const metadataMap = new Map(metadataResponse.data.map((crypto) => [crypto.id, crypto]));
+          this.cryptoData.forEach((crypto, index) => {
+            if (metadataMap.has(crypto.id)) {
+              crypto.image = metadataMap.get(crypto.id).image;
+              crypto.rank = index + 1;
+            }
+          });
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      },
+      sort(key) {
+        if (this.sortKey === key) {
+          this.sortTable *= -1;
+        } else {
+          this.sortKey = key;
+          this.sortTable = 1;
+        }
+      },
+    },
+    mounted() {
+      this.fetchData();
+    },
+    
 }
 </script>
 <style scoped>
 .container {
   position:absolute;
-  width: 600px;
+  max-width: 100px;
   top: 50%;
-  left: 50%;
+  left: 30%;
   transform: translate(-50%, -50%);
 }
-.table{
-  min-width: 100%;
-  table-layout: fixed;
+.crypto-coin{
+  max-width: 100px;
   border-collapse: collapse;
-  border: 1px solid #81adb7;
+  border: 1px solid #000000;
 }
-.table th{
-  font-size: 12px;
+.crypto-coin th{
+  font-size: 18px;
   white-space: nowrap;
-  padding: 8px 12px;
+  padding: 18px 12px;
+} 
+  .coin-icon {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+    inline-size: 20px;
+  }
+  .rank {
+    font-weight: bold;
+  }
+.background img{
+  /* width: 100vw;
+  height: 100vh; */
 }
-
 </style>
